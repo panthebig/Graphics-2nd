44bd5e84802372ce9f35c7569f5b8ea9f840ab20
pfilippos <pfilippos@csd.auth.gr>
2022-04-19 18:47:13 +0300

    Rotation with INITIAL scale of 2.5
    to do general translate and general scale

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 
 GLfloat angle =0;
+float a = 5.0;
+float initial = 1.0;
 
 
 void RotateFunc(){
@@ -24,10 +26,10 @@ void myinit(void){
 
     glNewList(1, GL_COMPILE);
         glBegin(GL_POLYGON);
-        glVertex3f(-50,-50,1);
-        glVertex3f(-50,50,1);
-        glVertex3f(50,50,1);
-        glVertex3f(50,-50,1);
+        glVertex3f( 1.0, 1.0, 1.0);
+        glVertex3f(-1.0, 1.0, 1.0);
+        glVertex3f(-1.0,-1.0, 1.0);
+        glVertex3f( 1.0,-1.0, 1.0);
         glEnd();
     glEndList();
 
@@ -38,7 +40,7 @@ void myinit(void){
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, -500.0);
+    glOrtho(-25, 25.0, -25, 25, -25.0, 25.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -52,44 +54,50 @@ void display( void )
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glTranslatef(250,250,0);
-    glRotatef(angle,1.0,1.0,1.0);
+    //glTranslatef(250,250,00);
+    glRotatef(angle, 1.0, 1.0, 1.0);
 
-    glPushMatrix(); //front plane
+    glPushMatrix(); //front plane //Red //First Scale than Rotate then Translate
+    glScalef(2.5, 2.5, 1);
     glColor3f(1.0, 0.0, 0.0);
     glCallList(1);
     glPopMatrix();
 
-    glPushMatrix(); //rear plane
-    glTranslatef(0.0,0.0,-100.0);
+    glPushMatrix(); //rear plane //Blue
+    glTranslatef(0.0,0.0,-5);
+    glScalef(2.5, 2.5, 1);
     glColor3f(0.0, 0.0, 1.0);
     glCallList(1);
     glPopMatrix();
 
-    glPushMatrix(); //Right side
-    glTranslatef(50.0,0.0,-50.0);
-    glRotatef(90,0.0,1.0,0.0);
+    glPushMatrix(); //Right side //Green
+    glTranslatef(1.5, 0.0, -1.5);
+    glRotatef(90, 0.0, 1.0, 0.0);
+    glScalef(2.5, 2.5, 1);
     glColor3f(0.0, 1.0, 0.0);
     glCallList(1);
     glPopMatrix();
 
-    glPushMatrix(); //Left side
-    glTranslatef(-50.0,0.0,-50.0);
-    glRotatef(90,0.0,1.0,0.0);
+    glPushMatrix(); //Left side -Cyan
+    glTranslatef(-3.5, 0.0, -1.5);
+    glRotatef(90, 0.0, 1.0, 0.0);
+    glScalef(2.5, 2.5, 1);
     glColor3f(0.0, 1.0, 1.0);
     glCallList(1);
     glPopMatrix();
 
-    glPushMatrix(); //Top side
-    glTranslatef(0.0,50.0,-50.0);
-    glRotatef(90,1.0,0.0,0.0);
+    glPushMatrix(); //Top side -Yellow
+    glTranslatef(0.0, 3.5, -1.5);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    glScalef(2.5, 2.5, 1);
     glColor3f(1.0, 1.0, 0.0);
     glCallList(1);
     glPopMatrix();
 
-    glPushMatrix(); //Bottom side
-    glTranslatef(0.0,-50.0,-50.0);
-    glRotatef(90,1.0,0.0,0.0);
+    glPushMatrix(); //Bottom side -Pink
+    glTranslatef(0.0, -1.5, -1.5);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    glScalef(2.5, 2.5, 1);
     glColor3f(1.0, 0.0, 1.0);
     glCallList(1);
     glPopMatrix();
@@ -104,11 +112,11 @@ int main(int argc, char** argv)
 {
 
     glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
-    glEnable(GL_DEPTH_TEST);
     glutInitWindowSize(500,500);
     glutInitWindowPosition(100,100);
     glutCreateWindow("Second Project");
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
 
